ef99b05736c8e82d1b0c818bc6783327b360fe7e
zhanghongwei06 <zhanghongwei06@58.com>
2022-07-04 15:36:53 +0800

    修改端口

--- service_controller/src/main/java/com/zhwcloud/controller/controller/SkyWalkingController.java
+++ service_controller/src/main/java/com/zhwcloud/controller/controller/SkyWalkingController.java
@@ -3,9 +3,12 @@ package com.zhwcloud.controller.controller;
 import com.zhwcloud.controller.service.SkyWalkingService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.ThreadContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 public class SkyWalkingController {
@@ -18,5 +21,22 @@ public class SkyWalkingController {
     public String skyWalkingIndex(){
         logger.info("skyWalking 接口被请求");
         return skyWalkingService.index();
+    }
+
+    @RequestMapping("logTrace")
+    public String skyWalkingLogTrace(){
+        ThreadContext.put("traceId", UUID.randomUUID().toString());
+
+        logger.info("参数校验");
+
+
+        logger.info("执行业务--1");
+
+
+        logger.info("执行业务--2");
+
+
+        logger.info("执行业务--3");
+        return "";
     }
 }
